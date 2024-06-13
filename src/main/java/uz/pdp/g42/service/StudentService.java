93e9518bab0e7aa031f2edc9849f92289f1b0e46
akmal <akmalnajimov70@gmail.com>
2024-06-13 14:03:01 +0500

    Servicelarni korib chiqib xatolarni belgilab idea berila
    Signed-off-by: akmal <akmalnajimov70@gmail.com>

--- src/main/java/uz/pdp/g42/service/StudentService.java
+++ src/main/java/uz/pdp/g42/service/StudentService.java
@@ -3,27 +3,54 @@ package uz.pdp.g42.service;
 import uz.pdp.g42.model.Student;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 public class StudentService implements BaseService<Student> {
+    private final List<Student> students = new ArrayList<>();
+
+    private static StudentService studentService;
+
+    public static StudentService getInstance() {
+        if (studentService == null) {
+            studentService = new StudentService();
+        }
+        return studentService;
+    }
+
     @Override
-    public Student create(Student student) throws IOException {
-        return null;
+    public Student create(Student student) {
+        student.setId(UUID.randomUUID());
+        students.add(student);
+        return student;
     }
 
     @Override
     public Student getById(UUID id) throws IOException {
-        return null;
+        return students.stream()
+                .filter(s -> s.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new IOException("Student not found"));
     }
 
     @Override
-    public boolean delete(UUID id) throws IOException {
-        return false;
+    public boolean delete(UUID id) {
+        Student student = students.stream()
+                .filter(s -> s.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+
+        if (student == null || !student.isActive()) {
+            return false;
+        }
+
+        student.setActive(false);
+        return true;
     }
 
     @Override
-    public List<Student> getList() throws IOException {
-        return List.of();
+    public List<Student> getList() {
+        return students;
     }
 }
